544c67d7284355ed897e4c61885710b37e076cb3
AbhayKasturia <akabhay1991@gmail.com>
2017-02-18 19:20:26 -0500

    added functionality for google maps returning the path

---
+++ gmaps.py
@@ -0,0 +1,15 @@
+import googlemaps
+from datetime import datetime
+
+
+# origin and dest should be in the format of a string with the lon and lat as 'lon,lat' 
+# sydney town hall :
+# '-33.8732,151.2061'
+
+def requiredArg (origin,dest):
+	gmaps = googlemaps.Client(key='AIzaSyCa0OIq7kHz5maDSHgvyZSosDg98Zc6kbI')	
+	print googlemaps.convert.latlng(origin)
+	direction_result = gmaps.directions(origin, 
+										dest,
+                                    	mode="walking")
+	print direction_result
