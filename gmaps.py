7b4a0a32227e45e0464aaff42c7f9c61e35ac25e
arima <arima@ccs.neu.edu>
2017-02-19 16:27:40 -0500

    Updated with polyline information

--- gmaps.py
+++ gmaps.py
@@ -1,6 +1,6 @@
 import googlemaps
 from datetime import datetime
-
+import polyline
 
 # origin and dest should be in the format of a string with the lon and lat as 'lon,lat' 
 # sydney town hall :
@@ -15,11 +15,16 @@ def requiredArg (origin,dest):
 								dest,
                                	mode="walking")
 	print len(direction_result[0])
+	print direction_result[0]
 	print len(direction_result[0]["legs"][0])
 	print len(direction_result[0]["legs"][0]["steps"])
+	ct = 0
 	for j in range(len(direction_result[0]["legs"])):
 		for i in range(len(direction_result[0]["legs"][0]["steps"])):
+			ct += 1
+			print polyline.decode(direction_result[0]["legs"][j]["steps"][i]["polyline"]["points"])
 			print direction_result[0]["legs"][j]["steps"][i]["start_location"] , direction_result[0]["legs"][j]["steps"][i]["duration"] , direction_result[0]["legs"][j]["steps"][i]["end_location"]
+	print ct
 	# print direction_result[0]["legs"][0]["start_location"] , direction_result[0]["legs"][0]["duration"] , direction_result[0]["legs"][0]["duration"]
 
 requiredArg ('-33.8732,151.2061' , '-33.0732,151.0061')
