7a84cf67a54e5fffae03045707dd9cc40b440a4b
AbhayKasturia <akabhay1991@gmail.com>
2017-02-18 19:49:44 -0500

    code to write to a sample json file

--- gmaps.py
+++ gmaps.py
@@ -9,7 +9,10 @@ from datetime import datetime
 def requiredArg (origin,dest):
 	gmaps = googlemaps.Client(key='AIzaSyCa0OIq7kHz5maDSHgvyZSosDg98Zc6kbI')	
 	print googlemaps.convert.latlng(origin)
-	direction_result = gmaps.directions(origin, 
-										dest,
-                                    	mode="walking")
-	print direction_result
+	file = open("./sample_json","w")
+	file.write(str(gmaps.directions(origin,
+								dest,
+                               	mode="walking")))
+	# print direction_result[0]
+
+requiredArg ('-33.8732,151.2061' , '-33.0732,151.0061')
